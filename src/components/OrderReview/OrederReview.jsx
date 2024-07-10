5ea04217199b15c8eb5661ed1b4cfd3cbeadaf54
kawsar-uddin <kawsaruddin450@gmail.com>
2024-07-10 10:47:49 +0600

    initial data load

---
+++ src/components/OrderReview/OrederReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrederReview = () => {
+    return (
+        <div>
+            <h2>Order Review Page!</h2>
+        </div>
+    );
+};
+
+export default OrederReview;
